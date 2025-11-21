a3cd3dae6dd2fe8bf2f1eb0e3675981115b2f55b
Abdvijay <abdvijay19@gmail.com>
2025-11-21 16:12:50 +0530

    find duplicates program added another way to solve

--- Zoho-Logic-Coding/16_Find-Duplicates.py
+++ Zoho-Logic-Coding/16_Find-Duplicates.py
@@ -4,15 +4,25 @@
 # Sample Output
 # The list [1, 2, 3, 1, 2, 4, 5] has these duplicate elements [1, 2]
 
-lst = list(map(int,input("Enter the list of numbers : ").split()))
-dic = {}
-result = []
+# lst = list(map(int,input("Enter the list of numbers : ").split()))
+# dic = {}
+# result = []
+# for i in lst:
+#     if i not in dic:
+#         dic[i] = 1
+#     else:
+#         dic[i] += 1
+# for i in dic.keys():
+#     if dic[i] > 1:
+#         result.append(i)
+# print(f"The list {lst} has these duplicate elements {result}")
+
+lst = list(map(int,input("Enter the list elements : ").split()))
+dup = []
+without_dup = []
 for i in lst:
-    if i not in dic:
-        dic[i] = 1
+    if i not in without_dup:
+        without_dup.append(i)
     else:
-        dic[i] += 1
-for i in dic.keys():
-    if dic[i] > 1:
-        result.append(i)
-print(f"The list {lst} has these duplicate elements {result}")
+        dup.append(i)
+print(f"The list {lst} has these duplicate elements {dup}")
