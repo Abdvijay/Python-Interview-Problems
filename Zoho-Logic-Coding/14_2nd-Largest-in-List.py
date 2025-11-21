79d26b70119f549e100bb4aee40e0af515391df9
Abdvijay <abdvijay19@gmail.com>
2025-11-21 16:06:18 +0530

    Changed the 2nd largest program in k th largest program

--- Zoho-Logic-Coding/14_2nd-Largest-in-List.py
+++ Zoho-Logic-Coding/14_2nd-Largest-in-List.py
@@ -4,6 +4,11 @@
 # Sample Output
 # The 2nd Largest Number of [1, 2, 3, 4, 5] is 4
 
+# Enter the number in list : 1 4 3 5 2
+# Enter the k th position : 1
+# The 2nd Largest Number of [1, 2, 3, 4, 5] is 5
+
 number = list(map(int,input("Enter the number in list : ").split()))
+k = int(input("Enter the k th position : "))
 number.sort()
-print(f"The 2nd Largest Number of {number} is {number[-2]}")
+print(f"The 2nd Largest Number of {number} is {number[-k]}")
