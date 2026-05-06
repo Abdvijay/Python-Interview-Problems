8390216d5ac8ce80a7fdbfa9891bcc22bea1a5f1
Abdvijay <abdvijay19@gmail.com>
2026-05-06 17:53:51 +0530

    Added one more way for converting capitilize

--- Zoho-Logic-Coding/30_Convert-To-Titlecase-Without-title.py
+++ Zoho-Logic-Coding/30_Convert-To-Titlecase-Without-title.py
@@ -4,9 +4,15 @@
 # Sample Output
 # After changing title case in sentence : Hi Vijay How Are You
 
+# sentence = input("Enter the sentence : ").split()
+# result = []
+# for word in sentence:
+#     title_case = word[0].upper() + word[1:].lower()
+#     result.append(title_case)
+# print(f"After changing title case in sentence : {' '.join(result)}")
+
 sentence = input("Enter the sentence : ").split()
 result = []
 for word in sentence:
-    title_case = word[0].upper() + word[1:].lower()
-    result.append(title_case)
+    result.append(word.capitalize())
 print(f"After changing title case in sentence : {' '.join(result)}")
