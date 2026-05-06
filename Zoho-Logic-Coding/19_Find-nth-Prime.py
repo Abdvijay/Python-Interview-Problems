3f92777bff00823b9e0032760df9ba73505ae138
Abdvijay <abdvijay19@gmail.com>
2026-05-06 16:53:52 +0530

    Added one more way to solve Find nth Prime

--- Zoho-Logic-Coding/19_Find-nth-Prime.py
+++ Zoho-Logic-Coding/19_Find-nth-Prime.py
@@ -24,30 +24,44 @@
 # number = int(input("Enter the nth value : "))
 # print(f"The {number}th prime number is : {nth_prime(number)}")
 
-import itertools
-nth_prime = int(input("Enter the number to find nth prime : "))
-prime = []
-for i in itertools.count():
-    flag = True
-    if i == 0 or i == 1:
-        continue
-    else:
-        for j in itertools.count():
-            if j == 0 or j == 1:
-                continue
-            else:
-                if i < j:
-                    break
-                else:
-                    if i % j == 0 and i != j:
-                        flag = False
-                        break
-    if flag:
-        prime.append(i)
+# import itertools
+# nth_prime = int(input("Enter the number to find nth prime : "))
+# prime = []
+# for i in itertools.count():
+#     flag = True
+#     if i == 0 or i == 1:
+#         continue
+#     else:
+#         for j in itertools.count():
+#             if j == 0 or j == 1:
+#                 continue
+#             else:
+#                 if i < j:
+#                     break
+#                 else:
+#                     if i % j == 0 and i != j:
+#                         flag = False
+#                         break
+#     if flag:
+#         prime.append(i)
 
-    if len(prime) == nth_prime:
-        break
-print(f'{nth_prime} prime number is {prime[-1]} and prime numbers are {prime}')
+#     if len(prime) == nth_prime:
+#         break
+# print(f'{nth_prime} prime number is {prime[-1]} and prime numbers are {prime}')
 
 # Enter the number to find nth prime : 6
 # 6 prime number is 13 and prime numbers are [2, 3, 5, 7, 11, 13]
+
+n = int(input("Enter n: "))
+result = []
+num = 2
+while len(result) < n:
+    flag = True
+    for i in range(2, num):
+        if num % i == 0:
+            flag = False
+            break
+    if flag:
+        result.append(num)
+    num += 1
+print(f'{n}th prime number is {result[-1]} and prime numbers are {result}')
