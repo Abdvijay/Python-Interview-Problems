425b08e2562fea30e5d62522c23335e8cb8f1dd8
Abdvijay <abdvijay19@gmail.com>
2026-06-01 17:42:48 +0530

    Convert titlecase prob removed wrong method

--- Zoho-Logic-Coding/30_Convert-To-Titlecase-Without-title.py
+++ Zoho-Logic-Coding/30_Convert-To-Titlecase-Without-title.py
@@ -4,15 +4,9 @@
 # Sample Output
 # After changing title case in sentence : Hi Vijay How Are You
 
-# sentence = input("Enter the sentence : ").split()
-# result = []
-# for word in sentence:
-#     title_case = word[0].upper() + word[1:].lower()
-#     result.append(title_case)
-# print(f"After changing title case in sentence : {' '.join(result)}")
-
 sentence = input("Enter the sentence : ").split()
 result = []
 for word in sentence:
-    result.append(word.capitalize())
+    title_case = word[0].upper() + word[1:].lower()
+    result.append(title_case)
 print(f"After changing title case in sentence : {' '.join(result)}")
