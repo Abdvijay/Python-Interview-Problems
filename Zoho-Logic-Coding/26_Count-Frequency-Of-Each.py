ed168d3c48414c4a877d86117d50f223df22947b
Abdvijay <abdvijay19@gmail.com>
2026-06-01 17:01:37 +0530

    Simplest way to count frequency of each char in a string using count method

--- Zoho-Logic-Coding/26_Count-Frequency-Of-Each.py
+++ Zoho-Logic-Coding/26_Count-Frequency-Of-Each.py
@@ -12,3 +12,11 @@ for chr in string:
     else:
         freq[chr] += 1
 print(f"The frequency of each char in a string {string} is {freq}")
+
+# Simplest way to count frequency of each char in a string using count method
+# string = input("Enter the string : ")
+# freq = {}
+# for letter in string:
+#     if letter not in freq:
+#         freq[letter] = string.count(letter)
+# print(f"Frequency of each character in a string {string} is {freq}")
