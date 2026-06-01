ff641407d26d8476b8b87f5859ffdcceb12c260b
Abdvijay <abdvijay19@gmail.com>
2026-06-01 17:17:24 +0530

    Another way to find longest word in a sentence using max function

--- Zoho-Logic-Coding/28_Find-Longest-Word-In-Sentence.py
+++ Zoho-Logic-Coding/28_Find-Longest-Word-In-Sentence.py
@@ -10,3 +10,14 @@ for word in sentence:
     if len(longest_word) < len(word):
         longest_word = word
 print(f"{' '.join(sentence)} in this sentence the longest word is {longest_word}")
+
+# Another way to find longest word in a sentence using max function
+# sentence = list(map(str,input("Enter the sentence : ").split(" ")))
+# result = {}
+# for word in sentence:
+#     word = word.strip()
+#     if len(word) not in result:
+#         result[len(word)] = [word]
+#     else:
+#         result[len(word)].append(word)
+# print(f"{result[max(result)]}")
