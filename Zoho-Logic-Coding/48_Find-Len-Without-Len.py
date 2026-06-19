2a789785428226a9b53ff453feeee8f094ca6c65
Abdvijay <abdvijay19@gmail.com>
2026-06-19 18:44:16 +0530

    48th problem bug solved

--- Zoho-Logic-Coding/48_Find-Len-Without-Len.py
+++ Zoho-Logic-Coding/48_Find-Len-Without-Len.py
@@ -4,8 +4,17 @@
 # Sample Output
 # The length of list elements [1, 2, 4, 6, 2] is 5
 
+# lst = list(map(int,input("Enter the list elements : ").split()))
+# count = 0
+# for i in lst:
+#     count += 1
+# print(f"The length of list elements {lst} is {count}")
+
+def counting(lst):
+    count = 0
+    for i in lst:
+        count += 1
+    return count
+
 lst = list(map(int,input("Enter the list elements : ").split()))
-count = 0
-for i in lst:
-    count += 1
-print(f"The length of list elements {lst} is {count}")
+print(f"The length of the list elements is {counting(lst)}")
